c6a27b1b80596eefbb5e4f48bf8d8b9fe9d2e122
vstasiv74 <vstasiv@gmail.com>
2020-12-17 18:16:01 +0200

    add header

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var open = document.getElementById("open")
+var close = document.getElementById("close")
+var links = document.getElementById("links")
+		// console.log(close)
+open.addEventListener("click", function () {
+	this.style.display = "none"
+	close.style.display = "block"
+	links.style.display = "block"
+})
+close.addEventListener("click", function () {
+	this.style.display = "none"
+	open.style.display = "block"
+	links.style.display = "none"
+})
